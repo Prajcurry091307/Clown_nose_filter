bfb10c6c1f410a936f0938967a8e2fa4a2c74497
Prajcurry091307 <77421995+Prajcurry091307@users.noreply.github.com>
2021-09-02 18:47:38 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,15 @@
 function preload(){
 }
 
-function setup(){
-    canvas=createCanvas(300,300);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(300,300);
-    video.hide();
+function setup()
+{
+    createCanvas(300,300);
     
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose',gotPoses);
 }
 
- function modelLoaded(){
-     console.log("PoseNet is Initialzed");
- }
-
- function gotPoses(results)
- {
-     if(results.length>0)
-     {
-         console.log(results);
-         console.log("nose x ="+ results[0].pose.nose.x);
-         console.log("nose y ="+ results[0].pose.nose.y);
-     }
- }
 function draw(){
-    image(video, 0, 0, 300, 300);
-}
-
-function take_snapshot(){
-    save('myFilterImage.png');
+    fill(255,0,0);
+    //strokWeight(6);
+    stroke(224, 22, 22);
+    circle(30,30,20);
 }
